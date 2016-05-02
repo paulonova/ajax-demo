3e60dac1a385f83ec9123d285693a7ce8cc56d89
Paulo Nova <paulonova@users.noreply.github.com>
2016-05-02 11:40:49 +0200

    Updated ajax.js

--- ajax.js
+++ ajax.js
@@ -1,1 +1,20 @@
+// JavaScript
+// Vi ska använda AJAX för att hämta innehållet på en hemsida
 
+// Vad är AJAX? 
+// Det är inte ett fotbollslag...
+// Det är inte ett diskmedel...
+// Det är inte ett programmeringsspråk...
+
+// ...det är en teknik, en kombination av saker, för att hämta data från URLs
+// AJAX - Asynchronous JavaScript and XML
+
+// Påbörja en hämtning
+var request = new XMLHttpRequest();
+
+request.onreadystatechange = function() {
+	console.log("Nånting hände!");
+}
+
+request.open('GET', 'http://www.google.com');
+request.send();
